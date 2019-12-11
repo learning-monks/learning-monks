316f8ff77fba8e4945ef22a73f50195ac72ee060
Gigin George <mail.gigin@gmail.com>
2019-12-11 15:26:56 +0530

    Header Updates

--- js/grayscale.js
+++ js/grayscale.js
@@ -28,7 +28,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    if ($("#mainNav").offset().top > $(".masthead").height()) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
